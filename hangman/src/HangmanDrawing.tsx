a380d825e677b96b473a5730e8720ef90e3956e7
Rusty <rjohnson5016@gmail.com>
2023-03-26 16:48:04 -0400

    Adding first version of hangman

---
+++ hangman/src/HangmanDrawing.tsx
@@ -0,0 +1,99 @@
+const HEAD = (
+  <div
+    style={{
+      height: "50px",
+      width: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      top: "50px",
+      right: "-30px",
+      position: "absolute",
+    }}
+  ></div>
+);
+const BODY = (
+  <div
+    style={{
+      height: "100px",
+      width: "10px",
+      top: "120px",
+      right: "0px",
+      background: "black",
+      position: "absolute",
+    }}
+  ></div>
+);
+const RIGHT_ARM = (
+  <div
+    style={{
+      height: "10px",
+      width: "100px",
+      top: "150px",
+      right: "-100px",
+      background: "black",
+      position: "absolute",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  ></div>
+);
+const LEFT_ARM = (
+  <div
+    style={{
+      height: "10px",
+      width: "100px",
+      top: "150px",
+      right: "10px",
+      background: "black",
+      position: "absolute",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  ></div>
+);
+const LEFT_LEG = (
+  <div
+    style={{
+      height: "10px",
+      width: "100px",
+      top: "210px",
+      right: "0px",
+      background: "black",
+      position: "absolute",
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  ></div>
+);
+const RIGHT_LEG = (
+  <div
+    style={{
+      height: "10px",
+      width: "100px",
+      top: "210px",
+      right: "-90px",
+      background: "black",
+      position: "absolute",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  ></div>
+);
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+  return (
+    <div style={{ position: "relative" }}>
+      {BODY_PARTS.slice(0, numberOfGuesses)}
+      <div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px" }}></div>
+      <div style={{ height: "50px", width: "10px", background: "black", position: "absolute", top: 0, right: 0 }}></div>
+      <div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px" }}></div>
+      <div style={{ height: "10px", width: "250px", background: "black" }}></div>
+    </div>
+  );
+}
